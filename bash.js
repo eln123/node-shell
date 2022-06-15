4c3f1ae8d18f171385c8d5e935f20c02d357f060
Ethan <elnair122@gmail.com>
2022-06-15 17:14:53 -0400

    finished

--- bash.js
+++ bash.js
@@ -1,9 +1,15 @@
+const { pwd } = require("./pwd");
+const { ls } = require("./ls");
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) =>{
     const cmd = data.toString().trim();
-
-    process.stdout.write('You typed: ' + cmd);
+    if (cmd === 'pwd') {
+       pwd()
+    } 
+    if (cmd === 'ls') {
+        ls()
+     } 
     process.stdout.write('\nprompt > ');
-    
 })
---
+++ pwd.js
@@ -0,0 +1,4 @@
+const pwd = () => {
+     process.stdout.write(__dirname)
+}
+module.exports = { pwd };
